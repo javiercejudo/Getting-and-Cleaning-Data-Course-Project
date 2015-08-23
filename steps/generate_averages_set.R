f82c720fa95d57f66e5652d78e9a0153ba866ff5
Javier Cejudo <javier@javiercejudo.com>
2015-08-23 23:29:54 +1000

    refactor: simplify final step

--- steps/generate_averages_set.R
+++ steps/generate_averages_set.R
@@ -1,17 +1,5 @@
-library(reshape2)
+library(plyr)
 
 step.generateAveragesSet <- function(tidyAll) {
-	meanBySubjectAndActivity <- function(accumulator, variable) {
-		means <- tapply(tidyAll[variable][, 1], list(tidyAll$subject, tidyAll$activity), mean)
-		molten <- melt(means, varnames = c("subject", "activity"), value.name = variable)
-
-		merge(accumulator, molten)
-	}
-
-	firstVarMeans <- tapply(tidyAll$tBodyAccMeanX, list(tidyAll$subject, tidyAll$activity), mean)
-	firstVar <- melt(firstVarMeans, varnames = c("subject", "activity"), value.name = "tBodyAccMeanX")
-
-	variables <- colnames(tidyAll)[4:length(colnames(tidyAll))];
-
-	Reduce(meanBySubjectAndActivity, variables, firstVar)
+	ddply(tidyAll, c("subject","activity"), numcolwise(mean))
 }
